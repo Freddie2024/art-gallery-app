060a407cbc610c3a929dfb04d031aac9c2f56448
YOUR NAME <cecilia@bartlewski.de>
2024-10-21 12:08:17 +0200

    added next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
+  images: {
+    domains: ["example-apis.vercel.app"], // Hinzufügen der Domain hier
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
