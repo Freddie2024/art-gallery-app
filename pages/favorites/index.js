232afec3af65d59da76357cea52642af51882ee2
FRIDA LEMKE <frida.lemke@gmx.de>
2024-12-03 13:34:44 +0100

    add component testing for favorite listing page

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -10,20 +10,6 @@ export default function FavoritesPage() {
   return (
     <div>
       <ArtPieces pieces={favoritePieces} title="Your Favorites" />
-      {/* Pass the favorite pieces as a prop */}
-      {/* {favoritePieces.length > 0 ? (
-        favoritePieces.map((piece) => (
-          <ArtPiecePreview
-            key={piece.slug}
-            image={piece.imageSource}
-            name={piece.name}
-            artist={piece.artist}
-            slug={piece.slug}
-          />
-        ))
-      ) : (
-        <p>No favorites yet!</p>
-      )} */}
     </div>
   );
 }
