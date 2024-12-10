32897bfde393c4f16485b181c20a85898b484e51
FRIDA LEMKE <frida.lemke@gmx.de>
2024-12-10 23:06:05 +0100

    remove border from spotlight-display

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -27,7 +27,6 @@ const ImageContainer = styled.div`
   width: 100%;
   height: 400px;
   position: relative;
-  border: 1px solid #ccc;
   background-color: white;
   padding: 10px;
   display: flex;
